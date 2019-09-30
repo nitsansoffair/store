871822755af4756d42f037df365eedf2e89ec5c7
Nitsan Soffair <nitsan.soffair@gmail.com>
2019-09-30 23:32:53 +0300

    Cart List

--- src/Context.js
+++ src/Context.js
@@ -7,9 +7,12 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct,
-        cart: [],
+        cart: storeProducts,
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     };
 
     componentDidMount() {
@@ -77,6 +80,22 @@ class ProductProvider extends Component {
         });
     };
 
+    increment = (id) => {
+        console.log('increment');
+    };
+
+    decrement = (id) => {
+        console.log('decrement');
+    };
+
+    removeItem = (id) => {
+        console.log('remove item');
+    };
+
+    clearCart = () => {
+        console.log('clear Cart');
+    };
+
     render() {
         const { children } = this.props;
 
@@ -86,7 +105,11 @@ class ProductProvider extends Component {
                 handleDetail: this.handleDetail,
                 addToCart: this.addToCart,
                 openModal: this.openModal,
-                closeModal: this.closeModal
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                 {children}
             </ProductContext.Provider>
